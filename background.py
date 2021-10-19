53b26110f277214c20b84d2b3ded208098d6f721
Paolo Lami <paololami2000@gmail.com>
2021-10-19 02:29:48 +0000

    final

--- background.py
+++ background.py
@@ -3,13 +3,34 @@
 # the stored PWM slider value to the GPIO output
 import RPi.GPIO as GPIO
 import time
-ledPin = 17
+
+ledPin1 = 17 #RED
+ledPin2 = 27  #BLUE
+ledPin3 = 22  #GREEN
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(ledPin, GPIO.OUT)
-pwm = GPIO.PWM(ledPin, 100) # PWM object on our pin at 100 Hz
-pwm.start(0) # start with LED off
+GPIO.setup(ledPin1, GPIO.OUT)
+GPIO.setup(ledPin2, GPIO.OUT)
+GPIO.setup(ledPin3, GPIO.OUT)
+
+pwm1 = GPIO.PWM(ledPin1, 100) # PWM object on our pin at 100 Hz
+pwm2 = GPIO.PWM(ledPin2, 100) # PWM object on our pin at 100 Hz
+pwm3 = GPIO.PWM(ledPin3, 100) # PWM object on our pin at 100 Hz
+
+pwm1.start(0) # start with LED off
+pwm2.start(0) # start with LED off
+pwm3.start(0) # start with LED off
+
 while True:
-  with open("led_pwm.txt", 'r') as f:
-    dutyCycle = float(f.read()) # read duty cycle value from file
-  pwm.ChangeDutyCycle(dutyCycle)
+  with open("red_pwm.txt", 'r') as f:
+    dutyCycle = float(f.read()) # read duty cycle value from red LED file
+  pwm1.ChangeDutyCycle(dutyCycle)
+  time.sleep(0.1)
+  with open("blue_pwm.txt", 'r') as f:
+    dutyCycle = float(f.read()) # read duty cycle value from blue LED file
+  pwm2.ChangeDutyCycle(dutyCycle)
+  time.sleep(0.1)
+  with open("green_pwm.txt", 'r') as f:
+    dutyCycle = float(f.read()) # read duty cycle value from green LED file
+  pwm3.ChangeDutyCycle(dutyCycle)
   time.sleep(0.1)
--- leds.py
+++ leds.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python37all
 import cgi
 data = cgi.FieldStorage()
-s1 = data.getvalue('slider1')
-with open('led_pwm.txt', 'w') as f:
-  f.write(str(s1))
+ledcolor = data.getvalue('option') #gets LED color selected from web page
+s1 = data.getvalue('slider1') #gets slider value for the pwn function
+if ledcolor == 'r': #if Red LED option is selected
+    with open('red_pwm.txt', 'w') as f:
+      f.write(str(s1))
+if ledcolor == 'b': #if Blue LED option is selected
+    with open('blue_pwm.txt', 'w') as f:
+      f.write(str(s1))
+if ledcolor == 'g': #if Green LED option is selected
+    with open('green_pwm.txt', 'w') as f:
+      f.write(str(s1))
+      
 print('Content-type: text/html\n\n')
 print('<html>')
-print('<form action="/cgi-bin/led_pwm.py" method="POST">')
+print('<form action="/cgi-bin/leds.py" method="POST">')
+print('<input type="radio" name="option" value="r"> Red LED <br>')
+print('<input type="radio" name="option" value="b"> Blue LED <br>')
+print('<input type="radio" name="option" value="g"> Green LED <br>')
 print('<input type="range" name="slider1" min="0" max="100" value="%s"><br>' % s1)
-print('<input type="submit" value=”Change LED brightness">')
+print('<input type="submit" value="Change LED brightness">')
 print('</form>')
 print('Brightness = %s' % s1)
 print('</html>')
